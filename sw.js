dd1664be7b8c571bab038bad9d56b1e8f41a2043
Tech1133 <technadzor@atomicmail.io>
2026-06-15 15:20:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // === ГЛАВНОЕ ПРАВИЛО: МЕНЯЙ ЭТУ ЦИФРУ ПРИ КАЖДОМ ОБНОВЛЕНИИ КОДА! ===
-const APP_VERSION = 'v18'; 
+const APP_VERSION = 'v189'; 
 const CACHE_NAME = `tracker-offline-${APP_VERSION}`;
 
 const ASSETS = [
